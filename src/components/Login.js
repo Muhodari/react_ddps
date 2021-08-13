e043fc35c21571867516804061f96edd58fdc4d7
Muhodari Sage <sagemuho@gmail.com>
2021-08-13 13:55:26 +0200

    added login component

---
+++ src/components/Login.js
@@ -0,0 +1,38 @@
+import React,{useState,useContext} from 'react'
+import { useNavigate } from 'react-router'
+import API from '../API'
+// components
+import Button from './Buttons'
+// styles
+import { Wrapper } from './Login.styles'
+// context
+import { Context } from '../context'
+
+const Login = () => {
+
+    const handleSubmit = ()=>{};
+    const handleInput = e =>{};
+
+    return(
+
+        <Wrapper>
+        <label>Username:</label>
+        <input
+          type='text'
+           value='state value'
+           name ='username'
+           onChange={handleInput}
+           />
+           <label>Password:</label>
+           <input 
+           type='password'
+           value='state value'
+           name='password'
+           onChange={handleInput}
+           />
+   <Button text='Login' callback={handleSubmit} />
+        
+        </Wrapper>
+    )
+
+}
